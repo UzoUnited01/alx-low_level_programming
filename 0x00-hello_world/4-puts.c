bdd357ea4c92a59969ad5a1a9c9e64a698ad0027
UzoUnited01 <ofonyiri@gmail.com>
2022-09-08 09:12:39 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main - print the string in the puts function
  *
  * Description: using the main function
- * this program prints  "Programming is like building a multilingual puzzle
+ * this program prints "Programming is like building a multilingual puzzle"
  * Return: 0
  */
 
