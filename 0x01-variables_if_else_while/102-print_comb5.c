25371d2090503caf9012dfbace824b83333282e4
UzoUnited01 <ofonyiri@gmail.com>
2022-09-11 14:17:27 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int nl = 0, n2;
+
+	while (nl <= 99)
+	{
+		n2 = nl;
+		while (n2 <= 99)
+		{
+			if (n2 != nl)
+			{
+				putchar((nl / 10) + 48);
+				putchar((nl % 10) + 48);
+				putchar(' ');
+				putchar((n2 / 10) + 48);
+				putchar((n2 % 10) + 48);
+
+				if (nl != 98 || n2 != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++n2;
+		}
+		++nl;
+	}
+	putchar('\n');
+	return (0);
+}
