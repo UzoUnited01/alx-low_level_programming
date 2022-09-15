d0429fca04a53612c394c133e5ded3057bbe462b
UzoUnited01 <ofonyiri@gmail.com>
2022-09-15 15:53:45 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,16 +8,26 @@
  */
 int main(void)
 {
+	int i;
+
 	for (int i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0 && (i % 5 == 0))
-				printf("Fizz Buzz ");
-				else if (i % 3 == 0)
-				printf("Fizz ");
-				else if (i % 5 == 0)
-				printf("Buzz ");
-				else
-				printf("%d\n", i);
-				}
-				return (0);
-				}
+		{
+			printf("Fizz Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d\n", i);
+		}
+	}
+	return (0);
+}
