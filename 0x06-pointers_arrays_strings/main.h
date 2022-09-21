7a830099a16f3858b1f39b13fbe1b082fbcebf5c
UzoUnited01 <ofonyiri@gmail.com>
2022-09-21 15:40:29 -0700

    strcat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
 /**
  * void prototypes()
@@ -20,5 +19,6 @@ char *rot13(char *)
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+int _putchar(char);
 
 #endif /*MAIN_H*/
