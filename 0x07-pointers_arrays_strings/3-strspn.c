5f86ca0393d55d058014ef0f9aefa0fa822c74e0
UzoUnited01 <ofonyiri@gmail.com>
2022-09-26 14:48:41 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,7 +3,7 @@
 /**
  * _strspn - gets lengthof a prefix substring
  * @s: string to check
- * @accept; string to check against
+ * @accept: string to check against
  *
  * Return: number of bytes of s in accept
  */
@@ -11,15 +11,15 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; s[i]; i++)
+	for (j = 0; *(s + j); j++)
 	{
-		for (j = 0; accept [j]; j++)
+		for (i = 0; *(accept + i); i++)
 		{
-			if (s[i] == accept[j])
+			if (*(s + j) == *(accept + i))
 				break;
 		}
-		if (!accept[j])
+		if (*(accept + i) == '\0')
 			break;
 	}
-	return (i);
+	return (j);
 }
