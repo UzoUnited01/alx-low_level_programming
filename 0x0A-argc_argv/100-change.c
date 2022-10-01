97c4bf0a7f6603899e80424560a73fc69353f8f0
UzoUnited01 <ofonyiri@gmail.com>
2022-09-30 17:50:11 -0700

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,42 +3,45 @@
 
 /**
  * main - Prints coints left
- * @args: Parameter Entry
- * @argv: One number of arguments
- * Return: 0 on one number of argument
+ * @argc: length of argv
+ * @argv: number of argument
+ * Return: Always 0
  */
 
-int main (int args, char *argv[])
+int main(int argc, char *argv[])
 {
-	int c, coins = 0;
+	int position, total, change, aux;
+	int coins[] = {25, 10, 5, 2, 1}; /* Array int */
 
-	if (args != 2)
+	position = total = change = aux = 0;
+
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	c = atoi(argv[1]);
-	if (c < 0)
+
+	total = atoi(argv[1]); /* convert str to int */
+
+	if (total <= 0)
 	{
-		printf("0\n");
+		printf('"0\n");
 		return (0);
 	}
-	for (; c >= 0;)
+
+	/* Declaring while */
+
+	while (coins[position] != '\0')
 	{
-		if (c >= 25)
-			c -= 25;
-		else if (c >= 10)
-			c -= 10;
-		else if (c >= 5)
-			c -= 5;
-		else if (c >= 2)
-			c -= 2;
-		else (c => 1)
-			c -= 1;
-		else
-			break;
-		coins += 1;
+		if (total >= coins[position])
+		{
+			aux = (total / coins[position]);
+			change += aux;
+			total -= coins[position] * aux;
+		}
+		position++;
 	}
-	printf("%d\n", coins);
+
+	printf("%d\n", change);
 	return (0);
 }
