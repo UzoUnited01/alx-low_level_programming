7f07f320f8beb0840a7f13e668752bd9ef2b7e49
UzoUnited01 <ofonyiri@gmail.com>
2022-10-20 10:14:29 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,29 +12,21 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	char *dup;
-	int len;
-	list_t *new;
+	list_t *new_node;
+	size_t n;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 		return (NULL);
 
-	dup = strdup(str);
-	if (dup == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
+	new_node->str = strdup(str);
 
-	for (len = 0; str[len];)
-		len++;
+	for (n = 0; str[n]; n++)
+		;
 
-	new->str = dup;
-	new->len = len;
-	new->next = *head;
+	new_node->len = n;
+	new_node->next = *head;
+	*head = new_node;
 
-	*head = new;
-
-	return (new);
+	return (*head);
 }
